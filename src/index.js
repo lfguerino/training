d398aa2d1cc547f190ce8176d44a865b380846d1
Luiz Filipe Guerino <lfguerino@gmail.com>
2023-04-29 20:48:46 -0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+/* eslint-disable prefer-destructuring */
+const http = require('http');
+const { URL } = require('url');
+const url = require('url');
+
+const routes = require('./routes');
+
+const server = http.createServer((request, response) => {
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+  url.parse('xxxx');
+
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
+
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndpoint = pathname.split('/').filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
+
+  const route = routes.find(
+    (routeObj) => routeObj.endpoint === pathname && routeObj.method === request.method,
+  );
+
+  if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.params = { id };
+
+    route.handler(request, response);
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.end(`Cannot ${request.method} ${pathname}`);
+  }
+});
+
+server.listen(3000, () => {
+  console.log('🔥 Server started at http://localhost:3000');
+});
---
+++ src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Mateus',
+  },
+  {
+    id: 2,
+    name: 'Rose',
+  },
+  {
+    id: 3,
+    name: 'José',
+  },
+  {
+    id: 4,
+    name: 'Filipe',
+  },
+];
