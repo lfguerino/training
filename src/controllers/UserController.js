375015e43a8bbd5f555aa5be909d9fd73683a9f3
Luiz Filipe Guerino <lfguerino@gmail.com>
2023-04-29 22:06:47 -0300

    bodyParser, Update and Delete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,5 @@
-const users = require('../mocks/users');
+/* eslint-disable prefer-const */
+let users = require('../mocks/users');
 
 module.exports = {
   listUsers(request, response) {
@@ -28,25 +29,53 @@ module.exports = {
   },
 
   createUser(request, response) {
-    let body = '';
+    const { body } = request;
 
-    request.on('data', (chunk) => {
-      body += chunk;
-    });
+    const lastUserId = users[users.length - 1].id;
 
-    request.on('end', () => {
-      body = JSON.parse(body);
+    const newUser = {
+      id: lastUserId + 1,
+      name: body.name,
+    };
 
-      const lastUserId = users[users.length - 1].id;
+    users.push(newUser);
 
-      const newUser = {
-        id: lastUserId + 1,
-        name: body.name,
-      };
-
-      users.push(newUser);
-
-      response.send(200, newUser);
-    });
+    response.send(200, newUser);
   },
+
+  updateUser(request, response) {
+    let { id } = request.params;
+    const { name } = request.body;
+
+    id = Number(id);
+
+    const userExists = users.find((user) => user.id === id);
+
+    if (!userExists) {
+      return response.send(400, { error: 'User not found' });
+    }
+
+    users = users.map((user) => {
+      if (user.id === id) {
+        return {
+          ...user,
+          name,
+        };
+      }
+      return user;
+    });
+
+    response.send(200, { id, name });
+  },
+
+  deleteUser(request, response) {
+    let { id } = request.params;
+
+    id = Number(id);
+
+    users = users.filter((user) => user.id !== id);
+
+    response.send(200, { deleted: true });
+  },
+
 };
